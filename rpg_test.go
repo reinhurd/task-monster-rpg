5998542f3decd720b0f2ff1b1da9dd5679461873
msbozhenov <roarus@yandex.ru>
2023-02-14 23:13:40 -0300

    Add two tests

--- rpg_test.go
+++ rpg_test.go
@@ -43,3 +43,98 @@ func Test_toCSV(t *testing.T) {
 		})
 	}
 }
+
+func Test_setNewLevel(t *testing.T) {
+	tests := []struct {
+		name     string
+		player   Player
+		expResXp int64
+		expResLv int64
+	}{
+		{
+			name: "normal_case",
+			player: Player{
+				Name:        "PersonOne",
+				Token:       "123456",
+				CurrentTask: "PHP",
+				Level:       1,
+				Xp:          100,
+				Health:      100,
+			},
+			expResXp: 100,
+			expResLv: 1,
+		},
+		{
+			name: "normal_case_2",
+			player: Player{
+				Name:        "PersonOne",
+				Token:       "123456",
+				CurrentTask: "PHP",
+				Level:       1,
+				Xp:          1000,
+				Health:      100,
+			},
+			expResXp: 0,
+			expResLv: 2,
+		},
+		{
+			name: "empty_case",
+			player: Player{
+				Name:        "",
+				Token:       "",
+				CurrentTask: "",
+				Level:       0,
+				Xp:          0,
+				Health:      0,
+			},
+			expResXp: 0,
+			expResLv: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.player.setNewLevel()
+			require.Equal(t, tt.expResXp, tt.player.Xp)
+			require.Equal(t, tt.expResLv, tt.player.Level)
+		})
+	}
+}
+
+func Test_completeTasksForXp(t *testing.T) {
+	tests := []struct {
+		name   string
+		player Player
+		expRes int64
+	}{
+		{
+			name: "normal_case",
+			player: Player{
+				Name:        "PersonOne",
+				Token:       "123456",
+				CurrentTask: "PHP",
+				Level:       1,
+				Xp:          100,
+				Health:      100,
+			},
+			expRes: 110,
+		},
+		{
+			name: "null_case",
+			player: Player{
+				Name:        "",
+				Token:       "",
+				CurrentTask: "",
+				Level:       0,
+				Xp:          0,
+				Health:      0,
+			},
+			expRes: 10,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.player.completeTasksForXp(DEFAULT_REWARD)
+			require.Equal(t, tt.expRes, tt.player.Xp)
+		})
+	}
+}
